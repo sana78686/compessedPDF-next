17d5251d41f14fca274af2d9e4249ac3e715d0a4
sankhan55 <sana.fatima@netopz.com>
2026-04-30 06:57:00 +0500

    font size set to 18px

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ export default {
   ],
   theme: {
     extend: {
+      fontSize: {
+        base: ["18px", { lineHeight: "1.55" }],
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
